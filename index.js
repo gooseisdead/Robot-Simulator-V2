cf39d6c500cb76e00964152fef1effa52b56b9b5
gooseisdead <gooseisdead@mac.com>
2020-10-28 10:59:47 -0400

    Done

--- index.js
+++ index.js
@@ -2,5 +2,36 @@ document.addEventListener("DOMContentLoaded", function() {
   // initializes the board
   init();
 
-  // ADD CODE HERE!
+  document.addEventListener("keydown", function(event) {
+    let tileContainer = document.querySelector("ul#moves-container")
+    let movesLi = document.createElement("li")
+      if (event.key === "ArrowRight") {
+        tileContainer.append(movesLi)
+        movesLi.innerHTML = "right"
+      } else if (event.key === "ArrowLeft") {
+        tileContainer.append(movesLi)
+        movesLi.innerHTML = "left"
+      } else if (event.key === "ArrowUp") {
+        tileContainer.append(movesLi)
+        movesLi.innerHTML = "up"
+      } else if (event.key === "ArrowDown") {
+        tileContainer.append(movesLi)
+        movesLi.innerHTML = "down"
+      } else {
+        console.log("Clang! Hit the wall!")
+      }
+  })
+
+  let moveButton = document.querySelector("#move-button")
+
+  moveButton.addEventListener("click", function(event) {
+      let moves = document.querySelectorAll("#moves-container li")
+      moves.forEach(function(movement) {
+        move(movement.innerText)
+        movement.remove()
+      })  
+  })
 });
+
+
+// const tileRobot = document.querySelector("tile-robot")
